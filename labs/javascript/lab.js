75bb3b69c4db6eeaab023b8b013257ef21023840
Kyle Wistrand <kyle.wistrand@gmail.com>
2017-10-13 11:43:20 -0700

    Began lab assignment

--- labs/javascript/lab.js
+++ labs/javascript/lab.js
@@ -1,2 +1,9 @@
 // Put your JavaScript lab code here!
 
+let x = "I like JavaScript more than I like to party.";
+
+console.log(x.substr(28, 100));
+
+let school = "I love Washington State Unviersity";
+
+console.log(school.replace("Washington State Unviersity", "The University of Washington"))
