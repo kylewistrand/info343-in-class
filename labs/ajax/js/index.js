3d2db0df79dadfcc809f9cfe24824dad10cbbfdc
Kyle Wistrand <kyle.wistrand@gmail.com>
2017-11-03 12:27:29 -0700

    started Pokemon assignment

--- labs/ajax/js/index.js
+++ labs/ajax/js/index.js
@@ -2,12 +2,19 @@
 "use strict";
 
 const POKEMON_API = "https://pokeapi.co/api/v2/pokemon/{name}/";
-const ERROR_ALERT = document.querySelector("#error-alert");
+const POKEMON = "pikachu";
+const ERROR_ALERT_DIV = document.querySelector("#error-alert");
+
+const NAME = document.querySelector("#name");
+const IMAGE = document.querySelector("#pokemon-pic");
+const TYPES = document.querySelector("#types");
+const MOVES = document.querySelector("#moves");
+const GAMES = document.querySelector("#games");
 
 function handleError(err) {
     console.error(err);
-    ERROR_ALERT.textContent = err.message;
-    ERROR_ALERT.classList.remove("d-none");
+    ERROR_ALERT_DIV.textContent = err.message;
+    ERROR_ALERT_DIV.classList.remove("d-none");
 }
 
 function handleResponse(response) {
@@ -21,3 +28,29 @@ function handleResponse(response) {
             });
     }
 }
+
+function upperFirstChar(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function makeList(data) {
+    let ul = document.createElement("ul");
+    //TODO for each:
+    //ul.appendChild();
+}
+
+function render(data) {
+    console.log(data);
+    NAME.textContent = upperFirstChar(data.name);
+    IMAGE.src = data.sprites.front_default;
+    IMAGE.alt = upperFirstChar(data.name);
+
+    data.types.forEach(function() {
+
+    });
+}
+
+fetch(POKEMON_API.replace("{name}", POKEMON))
+    .then(handleResponse)
+    .then(render)
+    .catch(handleError)
